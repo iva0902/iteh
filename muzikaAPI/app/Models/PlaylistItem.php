ad8a53328e60154cc8145594e36b600a4088147f
Iva Mijailovic <ivamijailovic16@gmail.com>
2024-03-19 18:45:24 +0100

    kreirani modeli

---
+++ muzikaAPI/app/Models/PlaylistItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlaylistItem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'playlist_items';
+
+    protected $fillable = [
+        'playlist_id',
+        'date_added',
+        'song_id',
+    ];
+
+    public function playlist()
+    {
+        return $this->belongsTo(Playlist::class);
+    }
+
+    public function song()
+    {
+        return $this->belongsTo(Song::class);
+    }
+}
---
+++ muzikaAPI/app/Models/Song.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+
+    protected $table = 'songs';
+
+    protected $fillable = [
+        'title',
+        'artist',
+        'url',
+        'duration',
+    ];
+
+    public function playlistItems()
+    {
+        return $this->hasMany(PlaylistItem::class);
+    }
+}
