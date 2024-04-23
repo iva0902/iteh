09ab8e4b27650928751bd5e08b30c575e0b6cfe0
Andjela Jovanovic <a.jovanovic31299@gmail.com>
2024-04-23 18:49:57 +0200

    pesme

--- muzika-ui/src/stranice/Player.jsx
+++ muzika-ui/src/stranice/Player.jsx
@@ -6,6 +6,15 @@ import kadSiSrecan from "../pesme/kadSiSrecan.mp3";
 import majmunskiPles from "../pesme/majmunskiPles.mp3";
 import maliParadajz from "../pesme/maliParadajz.mp3";
 import najlepsaMamaNaSvetu from "../pesme/najlepsaMamaNaSvetu.mp3";
+import nezgoda from "../pesme/nezgoda.mp3";
+import patofnice from "../pesme/patofnice.mp3";
+import razboleSeLisica from "../pesme/razboleSeLisica.mp3";
+import toJePileMalo from "../pesme/toJePileMalo.mp3";
+import traktorMileta from "../pesme/traktorMileta.mp3";
+import uspavankaZaMrava from "../pesme/uspavankaZaMrava.mp3";
+
+
+
 import {Col, Row} from "react-bootstrap";
 
 const Player = () => {
@@ -55,6 +64,55 @@ const Player = () => {
             url: najlepsaMamaNaSvetu,
             duration: 210
         }
+        ,
+        {
+            id: 7,
+            title: "Nezgoda",
+            artist: "Decije pesme 7",
+            url: nezgoda,
+            duration: 210
+        }
+        ,
+        {
+            id: 8,
+            title: "Patofnice",
+            artist: "Decije pesme 8",
+            url: patofnice,
+            duration: 210
+        }
+        ,
+        {
+            id: 9,
+            title: "Razbole se lisica",
+            artist: "Decije pesme 9",
+            url: razboleSeLisica,
+            duration: 210
+        }
+        ,
+        {
+            id: 10,
+            title: "To je pile malo",
+            artist: "Decije pesme 10",
+            url: toJePileMalo,
+            duration: 210
+        }
+        ,
+        {
+            id: 11,
+            title: "Traktor Mileta",
+            artist: "Decije pesme 11",
+            url: traktorMileta,
+            duration: 210
+        }
+        ,
+        {
+            id: 12,
+            title: "Uspavanka za mrava",
+            artist: "Decije pesme 12",
+            url: uspavankaZaMrava,
+            duration: 210
+        }
+
     ];
 
     const [currentSong, setCurrentSong] = useState(null);
